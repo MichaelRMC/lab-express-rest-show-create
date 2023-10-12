d884d699fef540f9ad1c5f851444d2e8f8752f8a
Michael Caldwell <michaelcaldwell@pursuit.org>
2023-10-12 02:14:54 -0400

    Lab

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -2,6 +2,11 @@ const express = require("express");
 const logs = express.Router()
 const logArray = require("../models/log.js")
 
+logs.post("/", (req, res) => {
+	logsArray.push(req.body);
+	res.status(303).json(logArray)
+})
+
 logs.get("/", (req, res) => {
 	res.json(logArray)
 })
@@ -15,10 +20,14 @@ logs.get("/:arrayIndex", (req, res) => {
 	}
 })
 	
-logs.post("/", (req, res) => {
-	logsArray.push(req.body);
-	res.status(200).json(logArray[logArray.length - 1]).send()
+logs.delete("/:arrayIndex", (req, res) => {
+	const {arrayIndex} = req.params
+	 if (logArray[arrayIndex]) {
+			const removedLog = logArray.splice(arrayIndex, 1);
+			res.status(303).json(removedLog);
+		} else {
+			res.status(404).redirect();
+		}
 })
-
 
 module.exports = logs
